484372fbd71f6b1770d5d35d9929ee4cfe23b3af
Joel Schecheleski <joel.jsp@gmail.com>
2020-09-22 16:03:28 -0300

    Adicionado nova tela de itens

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:food/componentes/category_item.dart';
+import 'package:food/data/dummy_data.dart';
+
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent( // delega ao MaxCrossAsis a quantidade de elementos por linha
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2, // proporção
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
       ),
-      children: <Widget>[
-        //
-        Card(
-          child: Text('Olá'),
-        )
-      ],
+      body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent( // delega ao MaxCrossAsis a quantidade de elementos por linha
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2, // proporção
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20
+          ),
+          children: DUMMY_CATEGORIES.map((cat) {
+            return CategoryItem(cat);
+          }).toList()
+      ),
     );
   }
 }
